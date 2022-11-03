d82cc1a840b713c090667092f8e22cbfe802dbe9
NCLAN\nltt <tamnlt2000@gmail.com>
2022-11-03 15:48:59 +0700

    add swagger

--- src/main/java/com/heroku/demo/controller/HelloController.java
+++ src/main/java/com/heroku/demo/controller/HelloController.java
@@ -2,13 +2,21 @@ package com.heroku.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping(value = HelloController.ROOT_PATH)
 public class HelloController {
+    public static final String ROOT_PATH = "/hello";
 
-    @GetMapping("/")
+    @GetMapping("/spring")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/world")
+    public String newindex() {
+        return "Hello World!";
     }
 
 }
