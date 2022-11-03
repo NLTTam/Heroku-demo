d24b6b2e039004dd1228c528ab007deaef23cafc
NCLAN\nltt <tamnlt2000@gmail.com>
2022-11-03 14:59:41 +0700

    init 2

--- src/main/java/com/heroku/demo/DemoApplication.java
+++ src/main/java/com/heroku/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.simple.demo;
+package com.heroku.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/heroku/demo/DemoApplicationTests.java
+++ src/test/java/com/heroku/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.simple.demo;
+package com.heroku.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
